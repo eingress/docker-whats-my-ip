8df8a10b6a6cbbe2e7092930d1fbaf4423d24486
Eingress Ltd <scott@eingress.io>
2026-06-10 13:28:08 +0100

    Fix handler: drop log.Fatal, return 400 on missing header, add newline

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func getIP(w http.ResponseWriter, r *http.Request) {
 	ip := r.Header.Get("Cf-Connecting-Ip")
-
-	_, err := io.WriteString(w, ip)
-	if err != nil {
-		log.Fatal(err)
+	if ip == "" {
+		http.Error(w, "no Cf-Connecting-Ip header", http.StatusBadRequest)
+		return
 	}
+	fmt.Fprintln(w, ip)
+}
 
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "10101"
+	}
 	http.HandleFunc("/", getIP)
-	log.Fatal(http.ListenAndServe(":10101", nil))
+	http.HandleFunc("/health", health)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
